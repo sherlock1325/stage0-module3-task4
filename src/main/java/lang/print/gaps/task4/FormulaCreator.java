d39499fcfaf667dc8b55c0f0c2bc12e958aa7019
sherlock1325 <106594627+sherlock1325@users.noreply.github.com>
2022-11-04 07:34:12 -0700

    module-task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task4;
 
 public class FormulaCreator {
+    private static int a;
+    private static int b;
+
     public static void main(String[] args) {
+        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+        System.out.println(c + h + a + r);
     }
 }
